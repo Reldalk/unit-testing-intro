1c9c9d7f3935afefec3aa96b64e7bdb4789b91f8
Reldalk <Reldalk@gmail.com>
2018-10-09 15:02:57 -0400

    console.log works

---
+++ config.js
@@ -0,0 +1,2 @@
+'use strict';
+module.exports.PORT = process.env.PORT || 8080;
--- server.js
+++ server.js
@@ -5,4 +5,12 @@ const data = require('./db/notes');
 
 console.log('Hello Noteful!');
 
+
 // INSERT EXPRESS APP CODE HERE...
+const express = require('express');
+
+const app = express();
+
+const {PORT} = require('./config');
+
+app.listen(PORT, function (){ console.log(`Your app is listening on port ${PORT}`); });
