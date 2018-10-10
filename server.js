026b5f3f9b78d1524d63b4b1a90bc2f86a3295c8
Reldalk <Reldalk@gmail.com>
2018-10-09 21:25:09 -0400

    finished activities

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 // Load array of notes
 const data = require('./db/notes');
-console.log('Hello Noteful!');
+const simDB = require('./db/simDB');  // <<== add this
+const notes = simDB.initialize(data); // <<== and this
 
 
 // INSERT EXPRESS APP CODE HERE...
@@ -11,8 +12,57 @@ const logger = require('./middleware/logger');
 const app = express();
 app.use(logger);
 
-app.get('/boom', (req, res, next) => {
-  throw new Error('Boom!!');
+app.use(express.static('public'));
+app.use(express.json());
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+app.get('/api/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+  notes.find(id, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
+  });
+});
+
+app.get('/api/notes', (req, res, next) => {
+  const { searchTerm } = req.query;
+  console.log('api/notes' + searchTerm);
+  notes.filter(searchTerm, (err, list) => {
+    if (err) {
+      return next(err); // goes to error handler
+    }
+    res.json(list); // responds with filtered array
+  });
 });
 
 app.use(function (req, res, next) {
@@ -28,6 +78,7 @@ app.use(function (err, req, res, next) {
     error: err
   });
 });
+
 
 const {PORT} = require('./config');
 
