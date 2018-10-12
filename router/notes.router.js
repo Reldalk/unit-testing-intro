cececa9c655e82b9383cd94273941b04d8ae54c8
Reldalk <Reldalk@gmail.com>
2018-10-12 14:43:51 -0400

    test

--- router/notes.router.js
+++ router/notes.router.js
@@ -6,54 +6,56 @@ const notes = simDB.initialize(data); // <<== and this
 
 const router = express.Router();
 
-
-router.put('/id', (req, res, next) => {
+router.put('/:id', (req, res, next) => {
   const id = req.params.id;
-
   /***** Never trust users - validate input *****/
   const updateObj = {};
-  const updateFields = ['title', 'content'];
-  updateFields.forEach(field => {
+  const updateableFields = ['title', 'content'];
+  updateableFields.forEach(field => {
     if (field in req.body) {
       updateObj[field] = req.body[field];
     }
   });
-
-  notes.update(id, updateObj, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
+  /***** Never trust users - validate input *****/
+  if (!updateObj.title) {
+    const err = new Error('Missing `title` in request body');
+    err.status = 400;
+    return next(err);
+  }
+  notes.update(id, updateObj)
+    .then(item => {
+      if (item) {
+        res.json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => next(err));
 });
 
 router.get('/:id', (req, res, next) => {
   const id = req.params.id;
-  notes.find(id, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.json(item);
-    } else {
-      next();
-    }
-  });
+  notes.find(id)
+    .then(item => {
+      if (item) {
+        res.json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 router.get('/', (req, res, next) => {
   const { searchTerm } = req.query;
   console.log('api/notes' + searchTerm);
-  notes.filter(searchTerm, (err, list) => {
-    if (err) {
-      return next(err); // goes to error handler
-    }
-    res.json(list); // responds with filtered array
-  });
+  notes.filter(searchTerm)
+    .then(list => {
+      res.json(list);
+    })
+    .catch(err => next(err));
 });
 
 // Post (insert) an item
@@ -67,28 +69,34 @@ router.post('/', (req, res, next) => {
     err.status = 400;
     return next(err);
   }
-
-  notes.create(newItem, (err, item) => {
-    if (err) {
-      return next(err);
-    }
-    if (item) {
-      res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
-    } else {
-      next();
-    }
-  });
+  notes.create(newItem)
+    .then(item => {
+      if (item) {
+        res.location(`http://${req.headers.host}/notes/${item.id}`).status(201).json(item);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 router.delete('/:id', (req, res, next) =>{
   const {id} = req.params;
   console.log(id);
-  notes.delete(id, err => {
-    if (err) {
-      return next(err);
-    }
-    res.sendStatus(204);
-  });
+  notes.delete(id)
+    .then(item => {
+      console.log(item);
+      if (item) {
+        res.sendStatus(204);
+      } else {
+        next();
+      }
+    })
+    .catch(err => {
+      next(err);
+    });
 });
 
 
--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(morgan('dev'));
 app.use(express.static('public'));
 app.use(express.json());
 
-app.use('/api/notes/', notesRouter);
+app.use('/api/notes', notesRouter);
 
 app.use(function (req, res, next) {
   var err = new Error('Not Found');
